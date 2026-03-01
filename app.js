af3a06d914cfcd7eeeac11593d337071ff9fcec0
ssrawat1 <ssr911999@gmail.com>
2026-03-01 17:16:15 +0530

    fix the cors bug

--- app.js
+++ app.js
@@ -16,30 +16,20 @@ await connectDb()
 await loadProducts()
 
 /* Cors Enable: */
-const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000'];
+const allowedOrigins = [process.env.CLIENT_URL];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
-app.use(cors({
-  origin: (origin, callback) => {
-    console.log({ origin })
-    if (origin === process.env.CLIENT_URL) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 
 /* Cookie Parser: */
 app.use(cookieParser(process.env.SESSION_SECRET))
