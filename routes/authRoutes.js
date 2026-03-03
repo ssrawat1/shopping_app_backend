39a209e32e8766d4840bff277a3a835224577cc2
ssrawat1 <ssr911999@gmail.com>
2026-03-03 16:50:24 +0530

    Add the rate limiter

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 const authRoutes = express.Router();
 
-import {handleSendOtp, handleVerifyOtp } from "../controllers/authController.js";
+import { handleSendOtp, handleVerifyOtp } from "../controllers/authController.js";
+import rateLimiter from "../utils/rateLimiter.js";
+
+/* Rate Limiter:  */
+const limiter = rateLimiter({
+  window: 15 * 60 * 1000,
+  limit: 7,
+  message: "Too Many Request"
+})
 
 /* User Otp Routes: */
-authRoutes.post("/send-otp", handleSendOtp);
+authRoutes.post("/send-otp", limiter, handleSendOtp);
 authRoutes.post("/verify-otp", handleVerifyOtp)
- 
+
 export default authRoutes
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { handleLogin, handleLogout, handleProfile, handleRegister } from "../controllers/userControllers.js"
+import rateLimiter from "../utils/rateLimiter.js";
 const userRoutes = express.Router();
 
+/* Rate Limiter:  */
+const limiter = rateLimiter({
+  window: 15 * 60 * 1000,
+  limit: 7,
+  message: "Too Many Request"
+})
+
+
 /* User Routes */
-userRoutes.post("/register", handleRegister)
-userRoutes.post("/login", handleLogin)
+userRoutes.post("/register", limiter, handleRegister)
+userRoutes.post("/login", limiter, handleLogin)
 userRoutes.get("/profile", handleProfile)
 userRoutes.post("/logout", handleLogout);
 
